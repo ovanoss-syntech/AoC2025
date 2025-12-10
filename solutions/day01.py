2d8f520ce18fe7430283cd509522e38695d4d3a7
ovaonss-syntech <orion.vanoss@myfuelmaster.com>
2025-12-09 19:47:35 -0500

    day01 part 1

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,12 +1,27 @@
 """Advent of Code 2025 - Day 1 Solution"""
 
-from utils.helpers import read_input
+from utils.helpers import read_lines
+from collections.abc import Callable
 
+def rotate(direction: str, amt: int) -> Callable[[int], int]:
+    def rotate_right(x: int) -> int:
+        return x + amt
+    def rotate_left(x: int) -> int:
+        return x - amt
+    if direction == "R":
+        return rotate_right
+    if direction == "L":
+        return rotate_left
 
-def part1(data: str) -> int:
-    """Solve part 1"""
-    # TODO: Implement part 1
-    return 0
+def part1(data: list) -> int:
+    zero_count = 0
+    lock_pos = 50
+    for instruction in data:
+        operation = rotate(instruction[0], int(instruction[1:]))
+        lock_pos = operation(lock_pos) % 100
+        if lock_pos == 0:
+            zero_count += 1
+    return zero_count
 
 
 def part2(data: str) -> int:
@@ -16,7 +31,7 @@ def part2(data: str) -> int:
 
 
 if __name__ == "__main__":
-    data = read_input(1)
+    data = read_lines(1)
     
     result1 = part1(data)
     print(f"Part 1: {result1}")
