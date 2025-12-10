3a9778ba8359d6c1c57e638f26922af9c1a5a72b
ovaonss-syntech <orion.vanoss@myfuelmaster.com>
2025-12-09 20:22:55 -0500

    add test helpers

--- utils/helpers.py
+++ utils/helpers.py
@@ -37,3 +37,40 @@ def read_numbers(day: int) -> list[int]:
         List of integers from the input file
     """
     return [int(line) for line in read_lines(day)]
+
+def read_test_input(day: int) -> str:
+    """Read test input file for a specific day.
+    
+    Args:
+        day: Day number (1-25)
+        
+    Returns:
+        Content of the test input file
+    """
+    with open(f"inputs/day{day:02d}_test.txt", "r") as f:
+        return f.read()
+
+
+def read_test_lines(day: int) -> list[str]:
+    """Read test input file and return as list of lines.
+    
+    Args:
+        day: Day number (1-25)
+        
+    Returns:
+        List of lines from the test input file
+    """
+    content = read_test_input(day)
+    return content.strip().split("\n")
+
+
+def read_test_numbers(day: int) -> list[int]:
+    """Read test input file and return as list of integers.
+    
+    Args:
+        day: Day number (1-25)
+        
+    Returns:
+        List of integers from the test input file
+    """
+    return [int(line) for line in read_test_lines(day)]
