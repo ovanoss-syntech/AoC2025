f091fece98d888543a608e54f3e7e7964f5ea080
ovaonss-syntech <orion.vanoss@myfuelmaster.com>
2025-12-09 20:23:04 -0500

    day01 part 2

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,33 +1,44 @@
 """Advent of Code 2025 - Day 1 Solution"""
 
-from utils.helpers import read_lines
-from collections.abc import Callable
-
-def rotate(direction: str, amt: int) -> Callable[[int], int]:
-    def rotate_right(x: int) -> int:
-        return x + amt
-    def rotate_left(x: int) -> int:
-        return x - amt
-    if direction == "R":
-        return rotate_right
-    if direction == "L":
-        return rotate_left
+from utils.helpers import read_lines, read_test_lines
 
 def part1(data: list) -> int:
     zero_count = 0
     lock_pos = 50
     for instruction in data:
-        operation = rotate(instruction[0], int(instruction[1:]))
-        lock_pos = operation(lock_pos) % 100
+        direction = instruction[0]
+        amt = int(instruction[1:])
+        if direction == "L":
+            lock_pos -= amt
+        if direction == "R":
+            lock_pos += amt
+        lock_pos = lock_pos % 100
         if lock_pos == 0:
             zero_count += 1
     return zero_count
 
 
-def part2(data: str) -> int:
-    """Solve part 2"""
-    # TODO: Implement part 2
-    return 0
+def part2(data: list) -> int:
+    zero_count = 0
+    lock_pos = 50
+    for instruction in data:
+        direction = instruction[0]
+        amt = int(instruction[1:])
+
+        if lock_pos == 0 and direction == "L":
+            zero_count -= 1
+
+        zero_count += amt // 100
+        amt = amt % 100
+        if direction == "L":
+            lock_pos -= amt
+        if direction == "R":
+            lock_pos += amt
+
+        if lock_pos >= 100 or lock_pos <= 0:
+            zero_count += 1
+        lock_pos = lock_pos % 100
+    return zero_count
 
 
 if __name__ == "__main__":
